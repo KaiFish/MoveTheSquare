a0dca6bca1841b1a3ee6112cc459850fb730feba
trash-bag-replica <43896095+trash-bag-replica@users.noreply.github.com>
2021-02-09 19:55:36 -0500

    square color and movement speed
    changed the color of the square and made the speed the square moves independent of framerate

--- MoveTheSquare/src/edu/ycp/cs320/movethesquare/controllers/GameController.java
+++ MoveTheSquare/src/edu/ycp/cs320/movethesquare/controllers/GameController.java
@@ -4,6 +4,15 @@ import edu.ycp.cs320.movethesquare.model.Game;
 import edu.ycp.cs320.movethesquare.model.Square;
 
 public class GameController {
+	private double lastFrame, currFrame, deltaTime;
+	
+	public GameController()
+	{		
+		currFrame = System.currentTimeMillis();
+		lastFrame = System.currentTimeMillis();
+		deltaTime = 0;
+	}
+	
 	public void computeSquareMoveDirection(Game game, Square square, double mouseX, double mouseY) {
 		if (mouseX >= 0 && mouseX < game.getWidth() && mouseY >= 0 && mouseY < game.getHeight()) {
 			double dx = mouseX - (square.getX() + square.getWidth()/2);
@@ -11,19 +20,22 @@ public class GameController {
 			
 			double moveX = 0, moveY = 0;
 			if (dx > 0) {
-				moveX = Game.MOVE_DIST;
-			} else {
-				moveX = -Game.MOVE_DIST;
+				moveX = Game.MOVE_DIST * deltaTime;
+			} else if(dx < 0){
+				moveX = -Game.MOVE_DIST * deltaTime;
 			}
 			if (dy > 0) {
-				moveY = Game.MOVE_DIST;
-			} else {
-				moveY = -Game.MOVE_DIST;
+				moveY = Game.MOVE_DIST * deltaTime;
+			} else if(dy < 0){
+				moveY = -Game.MOVE_DIST * deltaTime;
 			}
 			
 			game.setSquareDx(moveX);
 			game.setSquareDy(moveY);
 		}
+		currFrame = System.currentTimeMillis();
+		deltaTime = (currFrame - lastFrame) / 1000; //	time between frames in seconds
+		lastFrame = currFrame;
 	}
 
 	public void moveSquare(Game model, Square square) {
